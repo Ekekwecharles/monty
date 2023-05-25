225a9da7ecdb417e51b49c4af18515793295f85f
Charlessnow <ekekwecharles50@gmail.com>
2023-05-25 12:51:20 -0700

    implemented pint

--- opcodes.c
+++ opcodes.c
@@ -71,8 +71,8 @@ void _pint(stack_t **head, unsigned int line)
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line);
-		fclose(bus.file);
-		free(bus.content);
+		fclose(p_data.file);
+		free(p_data.data);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
